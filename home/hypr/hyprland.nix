44f5a0004843a87080c2aa34907ec337421af962
Alysara <alysara@proton.me>
2025-09-20 18:42:08 -0400

    Fixed hyprland issues

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -1,12 +1,17 @@
 { inputs, pkgs, fetchurl, lib, ... }:
 
 {
+  services.displayManager = {
+    sessionPackages = [
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    ];
+  }; 
   wayland.windowManager.hyprland = {
     enable = true;
+   
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     
-    package = inputs.hyperland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyperland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-
     settings.monitor = [ ",preferred,auto,1.25" ];
 
   };
