42f8913de40028af0f2fde7db482fc7eb70a9651
Alysara <alysara@proton.me>
2025-09-20 22:55:46 -0400

    Tried to add rofi to hyprland

--- home/rofi/rofi.nix
+++ home/rofi/rofi.nix
@@ -3,4 +3,15 @@
 {
   programs.rofi.enable = true;
 
+
+  wayland.windowManager.hyprland.settings = {
+    windowrule = [
+      "stayfocused, class:^Rofi$"
+      "norounding, class:^Rofi$"
+    ];
+
+    bindr = [
+      "$mainMod, Super_L, exec, rofi -show drun -pid /tmp/wofi-pid || pkill rofi"
+    ];
+  };
 }
