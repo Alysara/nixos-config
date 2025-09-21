6a3951aa932003d353f80780ab6cfbb8dc49db9f
Alysara <alysara@proton.me>
2025-09-21 16:54:22 -0400

    added zen browser flake v2

--- flake.nix
+++ flake.nix
@@ -8,8 +8,9 @@
 
     hyprland.url = "github:hyprwm/Hyprland";
     catppuccin.url = "github:catppuccin/nix";
-    
+
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }:
