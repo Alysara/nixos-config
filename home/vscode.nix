68d042dacd193d861ec035e75010c9b685b937bd
Alysara <alysara@proton.me>
2025-09-21 19:54:45 -0400

    Added vscode theme extensions

--- home/vscode.nix
+++ home/vscode.nix
@@ -5,7 +5,7 @@
     enable = true;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
-      tpstech.tpstech-catppuccin-vsc
+      catppuccin.catppuccin-vsc
     ];
   };
 }
