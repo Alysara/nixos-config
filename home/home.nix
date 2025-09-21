7a25af1b002856bd75ad15f837c83c53d1bad9fc
Alysara <alysara@proton.me>
2025-09-20 23:02:05 -0400

    Fixed syntax error

--- home/home.nix
+++ home/home.nix
@@ -33,7 +33,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs [
+  home.packages = with pkgs; [
     wl-clipboard
     yazi
 
