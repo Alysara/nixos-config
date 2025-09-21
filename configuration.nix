e55b97ecbf69358139409fc667da0af5f67f170a
Alysara <alysara@proton.me>
2025-09-21 00:41:40 -0400

    Removed discord and protonvpn

--- configuration.nix
+++ configuration.nix
@@ -104,9 +104,7 @@
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    discord
     git
-    protonvpn-gui
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
