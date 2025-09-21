669affeb7ab0030dde9169d9465869abc14faa83
Alysara <alysara@proton.me>
2025-09-20 22:30:32 -0400

    This is an automated commit

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -8,11 +8,6 @@
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     
-    environment.sessionVariables = {
-      WLR_NO_HARDWARE_CURSORS = "1";
-      NIXOS_OZONE_WL = "1";
-    };
-
     hardware = {
       opengl.enable = true;
       nvidia.modesetting.enable = true;
