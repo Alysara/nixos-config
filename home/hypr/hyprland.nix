4f5b21f80773f1ec2df687c197a0a58b99f01261
Alysara <alysara@proton.me>
2025-09-21 01:59:31 -0400

    Added a ton of configurations to the hyprland file

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -16,6 +16,85 @@
     
       xwayland.force_zero_scaling = true;
 
+      general = {
+        border_size = 1;
+        no_borders_on_floating = false;
+        gaps_in = 5;
+        gaps_out = 10;
+        float_gaps = 0;
+        gaps_workspaces = 0;
+        "col.inactive_border" = "0xff444444"; 
+        "col.active_border" = "0xffffffff";
+        "col.nogroup_border" = "0xffffaaff";
+        "col.nogroup_border_active" = "0xffff00ff";
+        layout = dwindle;
+        no_focus_fallback = false;
+        resize_on_border = true;
+        extend_border_grab_area = 15;
+        hover_icon_on_border = true;
+        allow_tearing = false;
+        resize_corner = 0;
+
+        snap = {
+          enabled = true;
+          window_gap = 10;
+          monitor_gap = 10;
+          border_overlap = false;
+          respect_gaps = false;
+        };
+      };
+
+      decoration = {
+        rounding = 10;
+        rounding_power = 2;
+        active_opacity = 0.9;
+        inactive_opacity = 0.5;
+        fullscreen_opacity = 0.95;
+        dim_modal = true;
+        dim_inactive = true;
+        dim_special = 0.2;
+        dim_around = 0.4;
+        # screen_shader = [insert path here]
+        border_part_of_window = true;
+
+        blur = {
+          enabled = true;
+          size = 8;
+          ignore_opacity = true;
+          new_optimizations = true;
+          xray = true;
+          noise = 0.0117;
+          contrast = 0.8916;
+          brightness = 0.8172;
+          vibrancy = 0.1696;
+          vibrancy_darkness = 0;
+          special = false;
+          popups = false;
+          popups_ignorealpha = 0.2;
+          input_methods = false;
+          input_methods_ignorealpha = 0.2;
+        };
+
+        shadow = {
+          enabled = true;
+          rangle = 4;
+          render_power = 3;
+          sharp = false;
+          ignore_window = true;
+          color = "0xee1a1a1a";
+          color_inactive = unset;
+          offset = "[0, 0]";
+          scale = 1;
+        };
+
+      };
+
+      input = {
+        kb_layout = us;
+      };
+
+
+
       "$mainMod" = "SUPER";
 
       bindm = [
