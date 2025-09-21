867e2e6a5ea2718a1eeafb2697be4b1e96179a25
Alysara <alysara@proton.me>
2025-09-20 22:29:45 -0400

    Added some hyprland settings

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -3,10 +3,21 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    nvidiaPatches = true;
    
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     
+    environment.sessionVariables = {
+      WLR_NO_HARDWARE_CURSORS = "1";
+      NIXOS_OZONE_WL = "1";
+    };
+
+    hardware = {
+      opengl.enable = true;
+      nvidia.modesetting.enable = true;
+    };
+
     settings = {
       monitor = [ ",preferred,auto,1.25" ];
 
