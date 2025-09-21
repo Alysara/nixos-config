ed944efbb043899abfacd5838f8a0a844207315b
Alysara <alysara@proton.me>
2025-09-20 22:03:11 -0400

    added

--- home/home.nix
+++ home/home.nix
@@ -34,10 +34,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-    wl-clipboard
+    pkgs.wl-clipboard
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
