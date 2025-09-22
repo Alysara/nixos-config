f65c918579348275f3cdec089e7e2763ffc56647
Alysara <alysara@proton.me>
2025-09-21 20:13:48 -0400

    Added intel stuff

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -6,7 +6,13 @@
     nvidiaBusId = "PCI:1:0:0";
   };
 
-  hardware.graphics.enable = true;
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vpl-gpu-rt
+    ]
+  };
   services.xserver.videoDrivers = [ "nvidia" "intel" ];
   hardware.nvidia.open = true;
 }
