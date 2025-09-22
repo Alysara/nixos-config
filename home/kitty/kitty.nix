585cc3a86a8e734c172ee45745af1acd801e93d2
Alysara <alysara@proton.me>
2025-09-21 23:19:21 -0400

    added ctrl + backspace to kitty

--- home/kitty/kitty.nix
+++ home/kitty/kitty.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 
 {
-  programs.kitty.enable = true;
+  programs.kitty = {
+    enable = true;
+    setttings = {
+      confirm_os_window_close = false;
+      enable_audio_bell = false;
+    };
+    extraConfig = ''
+      map ctrl+backspace send_text all \x17
+    ''
+  };
+
   catppuccin.kitty.enable = true;
 }
