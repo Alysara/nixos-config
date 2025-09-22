106ebfb68a41cc6d5841d3f1aec59f62b66901b5
Alysara <alysara@proton.me>
2025-09-22 14:35:26 -0400

    Trying to fix conflict

--- configuration.nix
+++ configuration.nix
@@ -166,8 +166,8 @@
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
 
-  # powerManagement.enable = true;
-  # services.thermald.enable = true;
-  services.tlp.enable = true;
+  powerManagement.enable = true;
+  services.thermald.enable = true;
+  # services.tlp.enable = true;
 
 }
