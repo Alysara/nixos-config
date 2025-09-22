82270ba7fe7600e8ce55956427f60221bc72f818
Alysara <alysara@proton.me>
2025-09-21 21:41:07 -0400

    give blueman catppuccin

--- home/home.nix
+++ home/home.nix
@@ -84,6 +84,7 @@
   catppuccin.fish.enable = true;
 
   services.blueman-applet.enable = true;
+  catppuccin.blueman.enable = true;
 
   programs.vesktop.enable = true;
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
