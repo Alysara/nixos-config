dc5dab7854d42a3d4091fa9a85e22c6cd9fbde26
Alysara <alysara@proton.me>
2025-09-21 23:25:46 -0400

    added catppuccin flavor and accent

--- home/kitty/kitty.nix
+++ home/kitty/kitty.nix
@@ -3,10 +3,10 @@
 {
   programs.kitty = {
     enable = true;
-    setttings = {
-      confirm_os_window_close = false;
-      enable_audio_bell = false;
-    };
+    # setttings = {
+    #   confirm_os_window_close = false;
+    #   enable_audio_bell = false;
+    # };
     # extraConfig = ''
     #  map ctrl+backspace send_text all \x17
     # '';
