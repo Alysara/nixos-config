0aab5c9b7f01a85fe33b6daa9eca91fecdff479d
Alysara <alysara@proton.me>
2025-09-22 17:22:08 -0400

    testing settings in vscode nix

--- home/vscode.nix
+++ home/vscode.nix
@@ -3,9 +3,17 @@
 {
   programs.vscode = {
     enable = true;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      catppuccin.catppuccin-vsc
-    ];
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        catppuccin.catppuccin-vsc
+      ];
+
+      settings = {
+        "files.autoSave" = "afterDelay";
+        "workbench.colorTheme" = "Catppuccin Mocha";
+      };
+    };
+
   };
 }
