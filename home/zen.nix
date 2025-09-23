21db3c6c248dda8490a31a02ac26dcc587c67f8f
Alysara <alysara@proton.me>
2025-09-22 20:45:13 -0400

    Added ublock

--- home/zen.nix
+++ home/zen.nix
@@ -16,22 +16,22 @@
         "uBlock0@raymondhill.net" = "ublock-origin";
         # "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
       };
-      AutofillAddressEnabled = true;
-      AutofillCreditCardEnabled = false;
-      DisableAppUpdate = true;
-      DisableFeedbackCommands = true;
-      DisableFirefoxStudies = true;
-      DisablePocket = true;
-      DisableTelemetry = true;
-      DontCheckDefaultBrowser = true;
-      NoDefaultBookmarks = true;
-      OfferToSaveLogins = false;
-      EnableTrackingProtection = {
-        Value = true;
-        Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
-      };
+      # AutofillAddressEnabled = true;
+      # AutofillCreditCardEnabled = false;
+      # DisableAppUpdate = true;
+      # DisableFeedbackCommands = true;
+      # DisableFirefoxStudies = true;
+      # DisablePocket = true;
+      # DisableTelemetry = true;
+      # DontCheckDefaultBrowser = true;
+      # NoDefaultBookmarks = true;
+      # OfferToSaveLogins = false;
+      # EnableTrackingProtection = {
+      #   Value = true;
+      #   Locked = true;
+      #   Cryptomining = true;
+      #   Fingerprinting = true;
+      # };
     };
   };
 
