8a8c2356f9f46ecd553a7d71019b8c9fdd20a756
Alysara <alysara@proton.me>
2025-09-22 20:39:29 -0400

    Added ublock

--- home/zen.nix
+++ home/zen.nix
@@ -7,25 +7,33 @@
 
   programs.zen-browser = {
     enable = true;
-    policies = {
-      AutofillAddressEnabled = true;
-      AutofillCreditCardEnabled = false;
-      DisableAppUpdate = true;
-      DisableFeedbackCommands = true;
-      DisableFirefoxStudies = true;
-      DisablePocket = true;
-      DisableTelemetry = true;
-      DontCheckDefaultBrowser = true;
-      NoDefaultBookmarks = true;
-      OfferToSaveLogins = false;
-      EnableTrackingProtection = {
-        Value = true;
-        Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
+    policies = let mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
+      installation_mode = "force_installed";
+    });
+    in {
+      ExtensionSettings = mkExtensionSettings {
+        "uBlock0@raymondhill.net" = "uBlock Origin";
+        # "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
       };
+    AutofillAddressEnabled = true;
+    AutofillCreditCardEnabled = false;
+    DisableAppUpdate = true;
+    DisableFeedbackCommands = true;
+    DisableFirefoxStudies = true;
+    DisablePocket = true;
+    DisableTelemetry = true;
+    DontCheckDefaultBrowser = true;
+    NoDefaultBookmarks = true;
+    OfferToSaveLogins = false;
+    EnableTrackingProtection = {
+      Value = true;
+      Locked = true;
+      Cryptomining = true;
+      Fingerprinting = true;
     };
   };
+};
 
   # programs.zen-browser.policies = let
   #   mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
