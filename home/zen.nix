40e5913923c8fe55dfdffc4a9638fdf5168e3885
Alysara <alysara@proton.me>
2025-09-22 20:43:11 -0400

    Added ublock

--- home/zen.nix
+++ home/zen.nix
@@ -13,39 +13,27 @@
     });
     in {
       ExtensionSettings = mkExtensionSettings {
-        "uBlock0@raymondhill.net" = "uBlock Origin";
+        "uBlock0@raymondhill.net" = "ublock-origin";
         # "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
       };
-    AutofillAddressEnabled = true;
-    AutofillCreditCardEnabled = false;
-    DisableAppUpdate = true;
-    DisableFeedbackCommands = true;
-    DisableFirefoxStudies = true;
-    DisablePocket = true;
-    DisableTelemetry = true;
-    DontCheckDefaultBrowser = true;
-    NoDefaultBookmarks = true;
-    OfferToSaveLogins = false;
-    EnableTrackingProtection = {
-      Value = true;
-      Locked = true;
-      Cryptomining = true;
-      Fingerprinting = true;
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
     };
   };
-};
-
-  # programs.zen-browser.policies = let
-  #   mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
-  #     install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
-  #     installation_mode = "force_installed";
-  #   });
-  # in {
-  #   ExtensionSettings = mkExtensionSettings {
-  #     "wappalyzer@crunchlabz.com" = "wappalyzer";
-  #     "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
-  #   };
-  # };
 
 
 }
