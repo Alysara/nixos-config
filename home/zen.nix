35001b56bb4cdff54d5e5e7539b9c5bf03cabd91
Alysara <alysara@proton.me>
2025-09-22 20:57:15 -0400

    cleaned up file

--- home/zen.nix
+++ home/zen.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, config, ... }:
+{ inputs, ... }:
 
 {
   imports = [
@@ -14,26 +14,7 @@
     in {
       ExtensionSettings = mkExtensionSettings {
         "uBlock0@raymondhill.net" = "ublock-origin";
-        # "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
       };
-      # AutofillAddressEnabled = true;
-      # AutofillCreditCardEnabled = false;
-      # DisableAppUpdate = true;
-      # DisableFeedbackCommands = true;
-      # DisableFirefoxStudies = true;
-      # DisablePocket = true;
-      # DisableTelemetry = true;
-      # DontCheckDefaultBrowser = true;
-      # NoDefaultBookmarks = true;
-      # OfferToSaveLogins = false;
-      # EnableTrackingProtection = {
-      #   Value = true;
-      #   Locked = true;
-      #   Cryptomining = true;
-      #   Fingerprinting = true;
-      # };
     };
   };
-
-
 }
