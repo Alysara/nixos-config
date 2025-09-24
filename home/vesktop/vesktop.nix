c2467553b8a5640056886f1eebb489750035a307
Alysara <alysara@proton.me>
2025-09-24 17:23:55 -0400

    This is an automated commit

--- home/vesktop/vesktop.nix
+++ home/vesktop/vesktop.nix
@@ -2,5 +2,10 @@
 
 {
   xdg.configFile."vesktop/settings/quickCss.css".source = ./quick.css;
-  programs.vesktop.enable = true;
+  programs.vesktop = {
+    enable = true;
+    plugins = [
+      "BetterFolders".enable = true;
+    ]
+  }
 }
