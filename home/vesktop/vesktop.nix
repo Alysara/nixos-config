7b2222e1194d9c43d37bbdcd3a4b502111120390
Alysara <alysara@proton.me>
2025-09-24 18:01:42 -0400

    This is an automated commit

--- home/vesktop/vesktop.nix
+++ home/vesktop/vesktop.nix
@@ -4,8 +4,37 @@
   xdg.configFile."vesktop/settings/quickCss.css".source = ./quick.css;
   programs.vesktop = {
     enable = true;
-    vencord.settings.plugins = {
-      "BetterFolders".enable = true;
+
+    settings = {
+      appBadge = false;
+      tray = false;
+    };
+
+    vencord.settings = {
+      transparent = true;
+      plugins = {
+        BetterFolders.enable = true;
+        MessageLatency.enable = true;
+        ShikiCodeBlocks.enable = true;
+        ShowHiddenThings.enable = true;
+        TypingTweaks.enable = true;
+        VolumeBooster.enable = true;
+        ClearURLs.enable = true;
+        ExpressionCloner.enable = true;
+        FriendsSize.enable = true;
+        MemberCount.enable = true;
+        MessageClickActions.enable = true;
+        NewGuildSettings.enable = true;
+        PlatformIndicators.enable = true;
+        RelationshipNotifier.enable = true;
+        SendTimestamps.enable = true;
+        ServerListIndicators.enable = true;
+        ShowHiddenChannels.enable = true;
+        TypingIndicator.enable = true;
+        ViewIcons.enable = true;
+        WhoReacted.enable = true;
+        YoutubeAdblock.enable = true;
+      };
     };
   };
 }
