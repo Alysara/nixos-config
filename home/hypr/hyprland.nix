33e0c13aba488a248e848c2b838c7b2b6cde884b
Alysara <alysara@proton.me>
2025-11-18 19:13:53 -0500

    Added step brightness functionality

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -1,6 +1,39 @@
-{ inputs, pkgs, fetchurl, lib, ... }:
+{ inputs, pkgs, config, fetchurl, lib, ... }:
 
-{
+let
+  stepBrightness = pkgs.writeShellScriptBin "step-brightness" ''
+    #!/usr/bin/env bash
+
+    steps=(0 1 2 3 5 10 15 25 40 60 80 95 100)
+
+    current=$(brightnessctl -d intel_backlight g)
+    max=$(brightnessctl -d intel_backlight m)
+    cur_percent=$((100 * current / max))
+
+    if [[ $1 == "up" ]]; then
+        for s in "''${steps[@]}"; do
+            if (( s > cur_percent )); then
+                brightnessctl -d intel_backlight s "''${s}%"
+                exit 0
+            fi
+        done
+    elif [[ $1 == "down" ]]; then
+        prev=0
+        for s in "''${steps[@]}"; do
+            if (( s >= cur_percent )); then
+                brightnessctl -d intel_backlight s "''${prev}%"
+                exit 0
+            fi
+            prev=$s
+        done
+    fi
+  '';
+in {
+  home.packages = [
+    pkgs.brightnessctl
+    stepBrightness
+  ];
+
   imports = [
     # ./hypredge.nix
     ./hyprlock.nix
@@ -136,8 +169,9 @@
         ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
         ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
         ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-        ",XF86MonBrightnessUp, exec, brightnessctl -d intel_backlight s 5%+"
-        ",XF86MonBrightnessDown, exec, brightnessctl -d intel_backlight s 5%-"
+        ",XF86MonBrightnessUp, exec, step-brightness up"
+        ",XF86MonBrightnessDown, exec, step-brightness down"
+
       ];
 
       bindl = [
