6b850b56a7b0ccb9e9d58729849e9e8e98d5b786
Alysara <alysara@proton.me>
2026-06-05 03:49:07 -0400

    More vim configurations, changes to hyprland, kitty, etc.

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -128,7 +128,7 @@ in
         };
 
         decoration = {
-          rounding = 10;
+          rounding = 0;
           rounding_power = 2;
           active_opacity = 1;
           inactive_opacity = 0.8;
